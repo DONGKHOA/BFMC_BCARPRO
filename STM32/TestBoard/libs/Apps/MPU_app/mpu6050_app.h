4565f5fc61caeebb2c625c478eba42fd4db3b3aa
DONGKHOA <thanhkhoa2k3@gmail.com>
2024-02-11 19:19:20 +0700

    update imu_app

--- STM32/TestBoard/libs/Apps/MPU_app/mpu6050_app.h
+++ STM32/TestBoard/libs/Apps/MPU_app/mpu6050_app.h
@@ -3,7 +3,7 @@
 
 
 #include "main.h"
-#include "STM32/TestBoard/libs/Drivers/MPU6050/mpu6050.h"
+#include "mpu6050.h"
 
 #define RADIAN_TO_DEGREE 180/3.141592f
 
@@ -12,6 +12,6 @@ const float dtt = (1.0 / 100.0); // sample rate
 const float RC = 0.35; //RC = 1.0/(cutofFreq * 2 * M_PI);
 const float alpha = dtt / (RC + dtt);
 
-void CalculateAccAngle(Angle_t* Angle, MPU6050_t* MPU6050)
+void CalculateAccAngle(Angle_t* Angle, MPU6050_t* MPU6050);
 
 #endif
